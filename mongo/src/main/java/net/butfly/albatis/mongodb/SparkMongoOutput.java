cded6be66eaf514c78e814e6ba3331d5da0b05b4
dangcx <15044774767@163.com>
2018-12-07 11:22:25 +0800

    fix compile error

--- mongo/src/main/java/net/butfly/albatis/mongodb/SparkMongoOutput.java
+++ mongo/src/main/java/net/butfly/albatis/mongodb/SparkMongoOutput.java
@@ -1,135 +1,144 @@
-//package net.butfly.albatis.mongodb;
-//
-//import java.util.List;
-//import java.util.Map;
-//import java.util.concurrent.BlockingQueue;
-//import java.util.concurrent.atomic.AtomicInteger;
-//import java.util.concurrent.atomic.AtomicLong;
-//
-//import org.apache.spark.api.java.JavaSparkContext;
-//import org.apache.spark.sql.SparkSession;
-//import org.bson.Document;
-//
-//import com.mongodb.bulk.BulkWriteResult;
-//import com.mongodb.client.MongoCollection;
-//import com.mongodb.client.model.InsertOneModel;
-//import com.mongodb.client.model.ReplaceOneModel;
-//import com.mongodb.client.model.ReplaceOptions;
-//import com.mongodb.client.model.WriteModel;
-//import com.mongodb.client.result.UpdateResult;
-//import com.mongodb.spark.MongoSpark;
-//
-//import net.butfly.albacore.io.URISpec;
-//import net.butfly.albacore.paral.Sdream;
-//import net.butfly.albacore.utils.collection.Colls;
-//import net.butfly.albacore.utils.collection.Maps;
-//import net.butfly.albatis.ddl.TableDesc;
-//import net.butfly.albatis.io.Rmap;
-//import net.butfly.albatis.spark.impl.SparkConf;
-//import net.butfly.albatis.spark.impl.SparkIO.Schema;
-//import net.butfly.albatis.spark.output.SparkSinkSaveOutput;
-//import net.butfly.albatis.spark.output.SparkWriting;
-//
-//@Schema("mongodb")
-//@SparkConf(key = "spark.mongodb.output.uri", value = "mongodb://127.0.0.1/FxxkMongoSpark.FakeCollection")
-//public class SparkMongoOutput extends SparkSinkSaveOutput implements SparkWriting, SparkMongo {
-//    private static final long serialVersionUID = -887072515139730517L;
-//    private static final int BATCH_SIZE = 250;
-//    private final String mongodbn;
-//    private final MongoSpark mongo;
-//
-//    public SparkMongoOutput(SparkSession spark, URISpec targetUri, TableDesc... table) {
-//        super(spark, targetUri, table);
-//        mongodbn = targetUri.getPathAt(0);
-//        mongo = MongoSpark.builder().options(options(null)).javaSparkContext(new JavaSparkContext(spark.sparkContext())).build();
-//    }
-//
-//    @Override
-//    public Map<String, String> options(String table) {
-//        Map<String, String> opts = mongoOpts(targetUri);
-//        opts.put("replaceDocument", "true");
-//        opts.put("maxBatchSize", Integer.toString(BATCH_SIZE));
-//        opts.put("localThreshold", "0");
-//        opts.put("writeConcern", "majority");
-//        if (null != table) opts.put("collection", table);
-//        return opts;
-//    }
-//
-//    @Override
-//    public boolean writing(long partitionId, long version) {
-//        logger().error("MongoDB writing, should init res here");
-//        return true;
-//    }
-//
-//    @Override
-//    public void enqueue(Sdream<Rmap> s) {
-//        Maps.ofQ(s, Rmap::table).forEach(this::write);
-//    }
-//
-//    // protected void write(Row row) {
-//    // Rmap r = row2rmap(row);
-//    // long rr = write(mongo.connector().acquireClient().getDatabase(mongodbn).getCollection(r.table()), r);
-//    // if (rr > 0) succeeded(rr);
-//    // else failed(Sdream.of());
-//    // }
-//
-//    private void write(String t, BlockingQueue<Rmap> docs) {
-//        long total = docs.size();
-//        logger().trace("MongoSpark upsert [" + total + "] to [" + t + "] with batch [" + BATCH_SIZE + "] starting.");
-//        long now = System.currentTimeMillis();
-//        MongoCollection<Document> col = mongo.connector().acquireClient().getDatabase(mongodbn).getCollection(t);
-//        AtomicLong succ = new AtomicLong();
-//        List<Rmap> fails = Colls.list();
-//        while (!docs.isEmpty()) {
-//            List<Rmap> batch = Colls.list();
-//            docs.drainTo(batch, BATCH_SIZE);
-//            if (!batch.isEmpty()) succ.addAndGet(write(col, batch));
-//        }
-//        logger().trace("MongoSpark upsert [" + total + "] to [" + t + "] with batch [" + BATCH_SIZE + "] finished, successed [" + succ.get()
-//                + "], failed [" + fails.size() + "], spent: " + now / 1000 + " ms.");
-//    }
-//
-//    private int write(MongoCollection<Document> col, List<Rmap> l) {
-//        AtomicInteger c = new AtomicInteger();
-//        s().statsOuts(l, rs -> {
-//            List<WriteModel<Document>> ws = Colls.list(rs, this::write);
-//            BulkWriteResult w = col.bulkWrite(ws);
-//            int cc = w.getInsertedCount() + (w.isModifiedCountAvailable() ? w.getModifiedCount() : 0) + w.getUpserts().size();
-//            c.addAndGet(cc);
-//        });
-//        return c.get();
-//    }
-//
-//    protected WriteModel<Document> write(Rmap r) {
-//        Document d = new Document(r);
-//        if (null == r.keyField() && !r.containsKey("_id")) return new InsertOneModel<Document>(d);
-//        Document q = null;
-//        if (null != r.keyField()) q = new Document(r.keyField(), r.key());
-//        else if (r.containsKey("_id")) q = new Document("_id", r.get("_id"));
-//        return new ReplaceOneModel<>(q, d, new ReplaceOptions().upsert(true));
-//    }
-//
-//    protected long write(MongoCollection<Document> col, Rmap r) {
-//        Document d = new Document(r);
-//        Document q = null;
-//        if (null != r.keyField()) q = new Document(r.keyField(), r.key());
-//        else if (r.containsKey("_id")) q = new Document("_id", r.get("_id"));
-//        Document qq = q;
-//        AtomicLong l = new AtomicLong();
-//        s().statsOut(r, rr -> {
-//            try {
-//                if (null != qq) {
-//                    UpdateResult u = col.replaceOne(qq, d, new ReplaceOptions().upsert(true));
-//                    l.set(u.getModifiedCount());
-//                } else {
-//                    col.insertOne(d);
-//                    l.set(1);
-//                }
-//            } catch (Exception ex) {
-//                logger().debug("MongoSpark fail: " + r, ex);
-//                l.set(-1);
-//            }
-//        });
-//        return l.get();
-//    }
-//}
+package net.butfly.albatis.mongodb;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.bson.Document;
+
+import com.mongodb.bulk.BulkWriteResult;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.InsertOneModel;
+import com.mongodb.client.model.ReplaceOneModel;
+import com.mongodb.client.model.ReplaceOptions;
+import com.mongodb.client.model.WriteModel;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.spark.MongoSpark;
+
+import net.butfly.albacore.io.URISpec;
+import net.butfly.albacore.paral.Sdream;
+import net.butfly.albacore.utils.collection.Colls;
+import net.butfly.albacore.utils.collection.Maps;
+import net.butfly.albatis.ddl.TableDesc;
+import net.butfly.albatis.io.Rmap;
+import net.butfly.albatis.spark.impl.SparkIO.Schema;
+import net.butfly.albatis.spark.output.SparkSinkSaveOutput;
+import net.butfly.albatis.spark.output.SparkWriting;
+
+@Schema("mongodb")
+public class SparkMongoOutput extends SparkSinkSaveOutput implements SparkWriting, SparkMongo {
+    private static final long serialVersionUID = -887072515139730517L;
+    private static final int BATCH_SIZE = 250;
+    private final String mongodbn;
+    private final MongoSpark mongo;
+
+    public SparkMongoOutput(SparkSession spark, URISpec targetUri, TableDesc... table) {
+        super(spark, targetUri, table);
+        mongodbn = targetUri.getPathAt(0);
+        mongo = MongoSpark.builder().options(options(null)).javaSparkContext(new JavaSparkContext(spark.sparkContext())).build();
+    }
+
+    @Override
+    public Map<String, String> options(String table) {
+        Map<String, String> opts = mongoOpts(targetUri);
+        opts.put("replaceDocument", "true");
+        opts.put("maxBatchSize", Integer.toString(BATCH_SIZE));
+        opts.put("localThreshold", "0");
+        opts.put("writeConcern", "majority");
+        if (null != table) opts.put("collection", table);
+        return opts;
+    }
+
+    @Override
+    public boolean writing(long partitionId, long version) {
+        logger().error("MongoDB writing, should init res here");
+        return true;
+    }
+
+    @Override
+    public void enqueue(Sdream<Rmap> sdream) {
+        Maps.ofQ(sdream, Rmap::table).forEach(this::write);
+    }
+
+    // protected void write(Row row) {
+    // Rmap r = row2rmap(row);
+    // long rr = write(mongo.connector().acquireClient().getDatabase(mongodbn).getCollection(r.table()), r);
+    // if (rr > 0) succeeded(rr);
+    // else failed(Sdream.of());
+    // }
+
+    private void write(String t, BlockingQueue<Rmap> docs) {
+        long total = docs.size();
+        logger().trace("MongoSpark upsert [" + total + "] to [" + t + "] with batch [" + BATCH_SIZE + "] starting.");
+        long now = System.currentTimeMillis();
+        MongoCollection<Document> col = mongo.connector().acquireClient().getDatabase(mongodbn).getCollection(t);
+        AtomicLong succ = new AtomicLong();
+        List<Rmap> fails = Colls.list();
+        while (!docs.isEmpty()) {
+            List<Rmap> batch = Colls.list();
+            docs.drainTo(batch, BATCH_SIZE);
+            if (!batch.isEmpty()) succ.addAndGet(write(col, batch));
+        }
+        logger().trace("MongoSpark upsert [" + total + "] to [" + t + "] with batch [" + BATCH_SIZE + "] finished, successed [" + succ.get()
+                + "], failed [" + fails.size() + "], spent: " + now / 1000 + " ms.");
+    }
+
+    private int write(MongoCollection<Document> col, List<Rmap> rmapList) {
+        AtomicInteger atomicInteger = new AtomicInteger();
+//      调用statsOuts方法,传入list,和一个函数.
+        s().statsOuts(rmapList, rs -> {
+            List<WriteModel<Document>> ws = Colls.list(rs, this::write);
+//          调用MOngoCollection的bulkWrite方法,传入上边的List
+            BulkWriteResult result = col.bulkWrite(ws);
+//          调用result的Count,求和
+            int cc = result.getInsertedCount() + (result.isModifiedCountAvailable() ? result.getModifiedCount() : 0) + result.getUpserts().size();
+            atomicInteger.addAndGet(cc);
+        });
+        return atomicInteger.get();
+    }
+
+
+
+//  传入一个rmap，返回一个WriteModel
+    protected WriteModel<Document> write(Rmap rmap) {
+//      传入rmap创建doc对象
+        Document doc = new Document(rmap);
+//        如果rmap的key是空并且rmap不包含_id,就返回InsertOneModel对象,同时传入上一步的doc对象
+        if (null == rmap.keyField() && !rmap.containsKey("_id")) return new InsertOneModel<Document>(doc);
+        Document newDoc = null;
+//      如果rmap的key属性不空,就传入keyfield和key用来创建doc对象.否则,如果rmap包含_id,就创建doc对象,只传入_id的映射
+        if (null != rmap.keyField()) newDoc = new Document(rmap.keyField(), rmap.key());
+        else if (rmap.containsKey("_id")) newDoc = new Document("_id", rmap.get("_id"));
+//      返回replaceOneModel对象
+        return new ReplaceOneModel<>(newDoc, doc, new ReplaceOptions().upsert(true));
+    }
+
+//  传入一个MongoC对象,一个rmap
+    protected long write(MongoCollection<Document> col, Rmap rmap) {
+        Document doc = new Document(rmap);
+        Document docResult = null;
+        if (null != rmap.keyField()) docResult = new Document(rmap.keyField(), rmap.key());
+        else if (rmap.containsKey("_id")) docResult = new Document("_id", rmap.get("_id"));
+        Document qq = docResult;
+        AtomicLong atom = new AtomicLong();
+        s().statsOut(rmap, rr -> {
+            try {
+                if (null != qq) {
+                    UpdateResult u = col.replaceOne(qq, doc, new ReplaceOptions().upsert(true));
+                    atom.set(u.getModifiedCount());
+                } else {
+                    col.insertOne(doc);
+                    atom.set(1);
+                }
+            } catch (Exception ex) {
+                logger().debug("MongoSpark fail: " + rmap, ex);
+                atom.set(-1);
+            }
+        });
+        return atom.get();
+    }
+}
--- mongo/src/test/java/com/hzcominfo/dataggr/spark/integrate/mongo/test/MongoTest.java
+++ mongo/src/test/java/com/hzcominfo/dataggr/spark/integrate/mongo/test/MongoTest.java
@@ -1,36 +1,45 @@
-//package com.hzcominfo.dataggr.spark.integrate.mongo.test;
-//
-//import java.util.Map;
-//
-//import org.apache.spark.SparkConf;
-//import org.apache.spark.api.java.JavaSparkContext;
-//import org.apache.spark.sql.SparkSession;
-//import org.bson.Document;
-//
-//import com.mongodb.spark.MongoSpark;
-//import com.mongodb.spark.config.ReadConfig;
-//import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
-//
-//import net.butfly.albacore.utils.collection.Maps;
-//
-//public class MongoTest {
-//
-//	public static void main(String[] args) {
-//		SparkConf conf = new SparkConf();
-//		conf.set("spark.mongodb.input.uri", "mongodb://user:pwd@localhost:80/db.tbl");
-//		SparkSession spark = SparkSession.builder().master("local[*]").appName("MongoTest").config(conf).getOrCreate();
-//
-//		Map<String, String> options = Maps.of();
-//		options.put("partitioner", "MongoSamplePartitioner");
-//		options.put("uri", "mongodb://devdb:Devdb1234@172.30.10.31:40012/devdb");
-//		options.put("database", "devdb");
-//		options.put("collection", "PH_ZHK_CZRK");
-//
+package com.hzcominfo.dataggr.spark.integrate.mongo.test;
+
+import java.util.Map;
+
+import net.butfly.albacore.io.URISpec;
+import net.butfly.albacore.paral.Sdream;
+import net.butfly.albatis.mongodb.SparkMongoOutput;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.bson.Document;
+
+import com.mongodb.spark.MongoSpark;
+import com.mongodb.spark.config.ReadConfig;
+import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
+
+import net.butfly.albacore.utils.collection.Maps;
+
+public class MongoTest {
+
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf();
+		conf.set("spark.mongodb.input.uri", "mongodb://user:pwd@localhost:80/db.tbl");
+		SparkSession spark = SparkSession.builder().master("local[*]").appName("MongoTest").config(conf).getOrCreate();
+
+		Map<String, String> options = Maps.of();
+		options.put("partitioner", "MongoSamplePartitioner");
+		options.put("uri", "mongodb://devdb:Devdb1234@172.30.10.31:40012/devdb");
+		options.put("database", "devdb");
+		options.put("collection", "PH_ZHK_CZRK");
+
+		URISpec uriSpec = new URISpec("mongodb://devdb:Devdb1234@172.30.10.31:40012/devdb");
+
+
+		SparkMongoOutput mongoOutput = new SparkMongoOutput(spark,uriSpec);
+		mongoOutput.enqueue(Sdream.of());
+
 //		JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 //		ReadConfig readConfig = ReadConfig.create(jsc).withOptions(options);
 //
 //		// Dataset<R> dataset = MongoSpark.load(jsc, readConfig).toDF();
 //		JavaMongoRDD<Document> rdd = MongoSpark.load(jsc, readConfig);// ZJHM
 //		System.out.println(rdd.first().toJson());
-//	}
-//}
+	}
+}
